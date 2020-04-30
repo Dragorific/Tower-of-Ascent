efe957061c717d38b0255051e70d9b6b68c57248
Umar Khan <dragorific@users.noreply.github.com>
2020-04-30 19:48:58 -0400

    fixed character collision box and added mesh collision to random tree and bush samples

---
+++ Tower of Ascent/Assets/Scripts/playerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    public CharacterController controller;
+    public Vector3 moveDir;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDir = new Vector3(Input.GetAxis("Horizontal")*moveSpeed, 0.0f, Input.GetAxis("Vertical")*moveSpeed);
+        if (Input.GetButtonDown("Jump")){
+            moveDir.y = jumpForce;
+        }
+
+        moveDir.y = moveDir.y + Physics.gravity.y;
+        controller.Move(moveDir*Time.deltaTime);
+    }
+}
